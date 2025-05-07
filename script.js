4b846050973c062003006f339d522696cc651f6c
RivaGiorgia <riva.giorgia@itisgrassi.edu.it>
2025-05-07 11:01:26 +0200

    esercizio1

---
+++ script.js
@@ -0,0 +1,23 @@
+///es1
+
+num = 65;
+console.log("numero:", num);
+function ControllaNumero(num){
+        if (num%2 == 0){
+                       console.log("numero pari");
+                       }
+        else           {
+                      console.log("numero dispari");
+                       }
+        if(num > 100) {
+                    alert("grande!");
+                      }
+}
+
+///es2
+
+///es3
+
+///es4
+
+///es5
